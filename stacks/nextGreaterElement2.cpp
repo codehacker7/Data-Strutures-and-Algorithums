1366c2dc4519789cc6a8a8b6c7cb12f957c47fb3
kavyansh bansal <kavyansheduv@gmail.com>
2023-08-22 14:39:34 -0700

    next Greater element 2

---
+++ stacks/nextGreaterElement2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> nextGreaterElements(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> nge(n, -1); // Initialize the result vector with -1
+    stack<int> st;
+
+    for (int i = 2 * n - 1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= nums[i % n]) {
+            st.pop();
+        }
+
+        if (i < n) {
+            if (!st.empty()) {
+                nge[i] = st.top();
+            }
+        }
+
+        st.push(nums[i % n]);
+    }
+
+    return nge;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    vector<int> result = nextGreaterElements(nums);
+
+    for (int i = 0; i < n; i++) {
+        cout << result[i] << " ";
+    }
+
+    return 0;
+}
