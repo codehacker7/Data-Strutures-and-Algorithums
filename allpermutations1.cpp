880171877be1e6a4d579fb57966df1cb1127fbdb
kavyansh bansal <kavyansheduv@gmail.com>
2023-08-28 12:32:18 -0700

    another Permuatation problem added

--- allpermutations1.cpp
+++ allpermutations1.cpp
@@ -20,6 +20,46 @@ void allPermutations(string s,int index,int length){
 
 
 }
+ bool checkInclusion(string s1, string s2) {
+
+        int freq[26] = {0};
+
+        for(int i = 0; i < s1.length(); i++){
+            freq[s1[i] - 'a']++;
+        }
+
+        int n = s1.length();
+
+        int i = 0;
+        int j = 0;
+
+        int freq2[26] = {0};
+
+        while(j < s2.length()) {
+            freq2[s2[j] - 'a']++;
+
+            if (j - i + 1 == n) {
+                int ct = 0;
+                for(int k = 0; k < 26; k++) {
+                    if (freq2[k] == freq[k]) {
+                        ct++;
+                    }
+                }
+
+                if (ct == 26) {
+                    return true;
+                } else {
+                    freq2[s2[i] - 'a']--;
+                    i++;
+                }
+            }
+            
+
+            j++;
+        }
+
+        return false;
+    }
 
 
 int main(){
