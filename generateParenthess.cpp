73bc2a5c9a1638b882a5ae30881325bc8de86638
kavyansh bansal <kavyansheduv@gmail.com>
2023-10-09 23:20:37 -0700

    improved code readibilty

--- generateParenthess.cpp
+++ generateParenthess.cpp
@@ -25,3 +25,38 @@ int main() {
     generateParenthesis(a, n, 0, 0, 0); // Call the generateParenthesis function
     return 0; // Return 0 to indicate successful program execution
 }
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// void generateParentensis(int n,int open,int close,string s){
+
+//     if(s.length() == 2*n){
+//         cout<<s<<endl;
+//         return;
+//     }
+
+//     if(open < n){
+//         s+="(";
+//         generateParentensis(n,open+1,close,s);
+//         s.pop_back();
+//     }
+
+//     if(close < open){
+//         s +=")";
+//       generateParentensis(n,open,close+1,s);
+//       s.pop_back();
+
+
+//     }
+// }
+
+// int main(){
+
+//     int n = 2;
+//     generateParentensis(n,0,0,"");
+
+//     return 0;
+// }
