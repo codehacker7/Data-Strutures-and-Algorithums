8b57ac039fb2343936a00c4359afa240760c547a
kavyansh bansal <kavyansheduv@gmail.com>
2023-10-11 01:07:14 -0700

    updated odd even linked list

--- linkedlist/oddEvenLinkedList.cpp
+++ linkedlist/oddEvenLinkedList.cpp
@@ -1,50 +1,80 @@
- struct ListNode {
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
     int val;
-    ListNode* next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    Node* next = NULL;
+    // constructor
+    Node(int v = 0, Node* n = NULL) {
+        val = v;
+        next = n;
+    }
 };
 
-class Solution {
-public:
-    ListNode* oddEvenList(ListNode* head) {
+int main() {
+    int number;
+    cin >> number;
 
-        if(head == NULL || head->next == NULL){
-            return head;
-        }
+    int val;
+    cin >> val;
 
-        ListNode* oddHead = head;
-        ListNode* evenHead = head->next;
-        ListNode* odd = oddHead;
-        ListNode* even = evenHead;
+    Node* head = new Node(val);
+    Node* temp = head;
 
-        while(odd != NULL && even != NULL){
+    for (int i = 1; i < number; i++) {
+        cin >> val;
+        temp->next = new Node(val);
+        temp = temp->next;
+    }
 
-            
-             if(even != NULL && odd != NULL){
-                odd->next = even->next;
+    temp = head;
+    Node* even = NULL;
+    Node* evenptr = NULL;
+    Node* odd = NULL;
+    Node* oddptr = NULL;
+
+    while (temp != NULL) {
+
+        if ((temp->val) % 2 == 0) {
+
+            if (even == NULL) {
+                even = temp;
+                evenptr = even;
+            } else {
+                even->next = temp;
+                even = even->next;
+            }
+        } else {
+
+            if (odd == NULL) {
+                odd = temp;
+                oddptr = odd;
+            } else {
+                odd->next = temp;
                 odd = odd->next;
-             }
-
-
-            if(even != NULL && odd != NULL){
-                
-                even->next = odd->next;
-                 even = even->next;
             }
 
         }
-       
-
-        ListNode* temp = oddHead;
-        while(temp->next != NULL){
-            temp = temp->next;
-        }
-
-        temp->next = evenHead;
-
-        return oddHead;
-        
+        temp = temp->next;
     }
-};
+
+    if (oddptr != NULL) {
+        odd->next = evenptr;
+    } else {
+        head = evenptr; // If there are no odd numbers, the head should point to the even numbers.
+    }
+    if (even != NULL) {
+        even->next = NULL; // dont forgot this part
+    }
+
+	temp = head;
+
+	while(temp != NULL){
+		cout<<temp->val<<" ";
+		temp = temp->next;
+	}
+   
+    return 0;
+}
+
