b844959e31a1ac773053f053011d14776eb810c1
kavyansh bansal <kavyansheduv@gmail.com>
2023-10-25 02:02:44 -0700

    print Range between K1 and K2 BST

--- tress/BSTtoLL.cpp
+++ tress/BSTtoLL.cpp
@@ -54,4 +54,12 @@ LinkedList BSTtoLL(node* root) {
         return l;
     }
 
+void printRange(node* root, int k1, int k2) {
+	if (root == NULL) return;
+
+	printRange(root->left, k1, k2);
+	if (root->data >= k1 and root->data <= k2) cout << root->data << " ";
+	printRange(root->right, k1, k2);
+}
+
 }
