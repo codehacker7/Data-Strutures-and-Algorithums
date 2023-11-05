fc50f45fbf8f551ca1677670e369079c03d30e62
kavyansh bansal <kavyansheduv@gmail.com>
2023-11-05 13:19:24 -0800

    triplet sum modified

--- arrays/tripletsum.cpp
+++ arrays/tripletsum.cpp
@@ -1,43 +1,40 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
 
-int main() {
+        sort(arr.begin(), arr.end());
+        int n = arr.size();
+        vector<vector<int>> finalans;
 
-	int a[] = { -1, 1, 0, 2, 4, -2, -2};
-	int n = sizeof(a) / sizeof(int);
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate elements
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
 
-	sort(a, a + n);
-		for (int i = 0; i < n - 2; ++i)
-		{
-			int j = i + 1;
-			int k = n - 1;
+            int j = i + 1;
+            int k = n - 1;
 
-			while ( (i > 0) and (i < n - 2) and (a[i] == a[i - 1])) {
-				i++;
-			}
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum == 0) {
+                    finalans.push_back({arr[i], arr[j], arr[k]});
+                    while (j < k && arr[j] == arr[j + 1]) {
+                        j++;
+                    }
+                    while (j < k && arr[k] == arr[k - 1]) {
+                        k--;
+                    }
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
 
-			while (j < k) {
-				if (a[i] + a[j] + a[k] == 0) {
-					cout << a[i] << ", " << a[j] << ", " << a[k]  << endl;
-					j++;
-					k--;
-					while (j < k and a[j] == a[j - 1]) {
-						j++;
-					}
-
-					while (k > j and a[k] == a[k + 1]) {
-						k--;
-					}
-				}
-				else {
-					if (a[i] + a[j] + a[k] < 0) {
-						j++;
-					}
-					else {
-						k--;
-					}
-				}
-			}
-		}
-}
+        return finalans;
+    }
+};
