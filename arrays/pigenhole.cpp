bc3d29b3f9738c00ca9928125e237d17834c8a64
kavyansh bansal <kavyansheduv@gmail.com>
2023-11-05 13:12:52 -0800

    pigenhole principle

---
+++ arrays/pigenhole.cpp
@@ -0,0 +1,38 @@
+// Pigeonhole
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a[] = {1, 0, 4, 2, 3};
+	int n = sizeof(a) / sizeof(int);
+
+	int prefixSum[100];
+
+	prefixSum[0] = a[0];
+
+
+    for(int i=1;i<n;i++){
+        prefixSum[i] = prefixSum[i-1] + a[i];
+    }
+
+    for(int i=0;i<n;i++){
+        prefixSum[i] = prefixSum[i] %n;
+    }
+
+    int cnt[10000];
+    cnt[0] = 1;
+
+    for(int i=0;i<n;i++){
+        cnt[prefixSum[i]]++;
+    }
+
+    int ans =0;
+    for (int i = 0; i < n; ++i)
+	{
+		ans += (cnt[i] * (cnt[i] - 1)) / 2;
+	}
+
+    return ans;
+
+}
