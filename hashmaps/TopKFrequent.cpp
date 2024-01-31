32b5176b6c384f0db29f27c4466edda767836cc2
kavyansh bansal <kavyansheduv@gmail.com>
2024-01-31 15:44:09 -0800

    updated top k frequent element

--- hashmaps/TopKFrequent.cpp
+++ hashmaps/TopKFrequent.cpp
@@ -2,39 +2,31 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
 
-        map<int,int> freq;
+        map<int,int> mp;
 
+        for(int i = 0; i < nums.size();i++){
+            int element = nums[i];
+            mp[element]++;
+        }
+
+        priority_queue<pair<int,int> > pq;
+
+        for(auto p : mp){
+            auto element = p.first;
+            auto frequency = p.second;
+            pq.push({frequency,element});
+        }
         vector<int> ans;
 
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]]++;
-        }
-
-        int count = 1;
-
-        while(count <= k){
-            int maxi = INT_MIN;
-            int finalelement = INT_MIN;
-            for(auto i: freq){
-                int element = i.first;
-                int freq = i.second;
-                if(freq > maxi){
-                    maxi = freq;
-                    finalelement = element;
-                }   
-            }
-
-            ans.push_back(finalelement);
-            auto it = freq.find(finalelement);
-            freq.erase(finalelement);
-
+        int count = 0;
+        while(count < k){
+            pair p = pq.top();
+            int element = p.second;
+            ans.push_back(element);
+            pq.pop();
             count++;
-
         }
 
-        return ans;
-
-          
-        
+        return ans;        
     }
 };
