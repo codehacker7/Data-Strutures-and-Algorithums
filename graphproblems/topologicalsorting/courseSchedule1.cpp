96c54c174e99b63fa8aac1d14acbfb6b7c0dcfdb
kavyansh bansal <kavyansheduv@gmail.com>
2024-02-19 21:14:39 -0800

    topo problems and cycle

---
+++ graphproblems/topologicalsorting/courseSchedule1.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int V = numCourses;
+        
+        vector<int> adj[V];
+
+        for(auto it :prerequisites){
+            adj[it[0]].push_back(it[1]);
+        }
+        
+        	
+	    // code here
+	    int indegree[V];
+
+        for(int i =0;i<V;i++){
+            indegree[i] = 0;
+        }
+	    
+	   for(int i =0;i<V;i++){
+	       for(auto it : adj[i]){
+	           indegree[it]++;
+	       }
+	   }
+	   
+	   queue<int> q ;
+	   for(int i=0;i<V;i++){
+	        if(indegree[i] == 0){
+	            q.push(i);
+	        }
+	   }
+	   
+	   vector<int> topo;
+	   while(!q.empty()){
+	       int node = q.front();
+	       q.pop();
+	       topo.push_back(node);
+	       
+	       for(auto it :adj[node]){
+	           indegree[it]--;
+	           if(indegree[it] == 0){
+	               q.push(it);
+	           }
+	       }
+	   }
+
+
+       if(topo.size() == V){
+           return true;
+       }
+	  
+    
+        return false;
+
+
+    }
+
+
+    
+};
