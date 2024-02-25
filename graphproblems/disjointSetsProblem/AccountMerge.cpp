877dd0b5c708a38168669f93779c2ae430d0154b
kavyansh bansal <kavyansheduv@gmail.com>
2024-02-24 23:37:18 -0800

    disjoint sets problems

---
+++ graphproblems/disjointSetsProblem/AccountMerge.cpp
@@ -0,0 +1,99 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution{
+  public:
+    vector<vector<string>> accountsMerge(vector<vector<string>> &details) {
+        // code here
+        int n = details.size();
+        unordered_map<string,int> mapMailNode;
+        DisjointSet ds(n);
+        for(int i =0;i < n;i++){
+            for(int j = 1; j < details[i].size();j++){
+                string mail = details[i][j];
+                if(mapMailNode.find(mail) == mapMailNode.end()){
+                    mapMailNode[mail] = i;
+                }else{
+                    ds.unionBySize(i,mapMailNode[mail]);
+                }
+            }
+        }
+        
+        
+       vector<vector<string>> mergedMail(n); 
+
+// ...
+
+        for(auto it : mapMailNode){
+            string mail = it.first;
+            int node = ds.findUPar(it.second);
+            mergedMail[node].push_back(mail); // Push the mail to the corresponding vector
+        }
+        
+        // ...
+        
+        vector<vector<string>> ans;
+        
+        for(int i = 0; i < n; i++){
+            if(mergedMail[i].empty()){
+                continue;
+            }
+            sort(mergedMail[i].begin(), mergedMail[i].end());
+            vector<string> temp;
+            temp.push_back(details[i][0]);
+            temp.insert(temp.end(), mergedMail[i].begin(), mergedMail[i].end());
+            ans.push_back(temp);
+        }
+        
+        return ans;
+
+        
+        
+    }
+};
